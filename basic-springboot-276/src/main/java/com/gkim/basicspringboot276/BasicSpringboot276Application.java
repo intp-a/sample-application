e925d797591d3c1b5f2c7611f1116ce53038e6d4
gkim <guardkim9@nate.com>
2022-12-15 05:26:38 +0000

    change sample app

--- basic-springboot-276/src/main/java/com/gkim/basicspringboot276/BasicSpringboot276Application.java
+++ basic-springboot-276/src/main/java/com/gkim/basicspringboot276/BasicSpringboot276Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.gkim.basicspringboot276;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BasicSpringboot276Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BasicSpringboot276Application.class, args);
 	}
 
 }
--- basic-springboot-276/src/test/java/com/gkim/basicspringboot276/BasicSpringboot276ApplicationTests.java
+++ basic-springboot-276/src/test/java/com/gkim/basicspringboot276/BasicSpringboot276ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.gkim.basicspringboot276;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BasicSpringboot276ApplicationTests {
 
 	@Test
 	void contextLoads() {
